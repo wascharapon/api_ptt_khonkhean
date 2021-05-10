e784a507bb2cc251ad54c59458ccb477090eb3b9
wascharapon akkhahad <48889077+wascharapon@users.noreply.github.com>
2021-05-11 00:39:41 +0700

    Update api_ptt_kku.php

--- api_ptt_kku.php
+++ api_ptt_kku.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 <?php
 
 $curl = curl_init();
@@ -15,10 +24,28 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTPHEADER => array(
     'Content-Type: application/json; charset=UTF-8'
   ),
-));
+)); //sProvinceCode เลือกจังหวัด
 
 $response = curl_exec($curl);
+$response = curl_exec($curl);
+$info = curl_getinfo($curl);
 
-curl_close($curl);
-$data=json_encode($response);
-echo $data;
+$data =json_decode ($response,true);
+//$array = json_decode(json_encode($data), True);
+$data_kku=$data['d'][9];
+$obj_kku = (object) $data_kku;
+//print_r($obj_kku);
+echo "".$obj_kku->sDistrict;
+echo "<br>nGasoline95 : ".$obj_kku->nGasoline95;
+echo "<br>nGasohol91 : ".$obj_kku->nGasohol91;
+echo "<br>nGasohol95 : ".$obj_kku->nGasohol95;
+echo "<br>nGasoholE20 : ".$obj_kku->nGasoholE20;
+echo "<br>nGasoholE85 : ".$obj_kku->nGasoholE85;
+echo "<br>nDiesel : ".$obj_kku->nDiesel;
+echo "<br>nHyForce : ".$obj_kku->nHyForce;
+echo "<br>nDieselB10 : ".$obj_kku->nDieselB10;
+echo "<br>nDieselB20 : ".$obj_kku->nDieselB20;
+echo "<br>nNGV : ".$obj_kku->nNGV;
+?>
+</body>
+</html>
